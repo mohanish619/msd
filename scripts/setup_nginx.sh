a151986ae2034128a0f3be79d2991a1b04031b3d
mohanish619 <75253869+mohanish619@users.noreply.github.com>
2024-10-11 20:14:11 +0530

    Update setup_nginx.sh

--- scripts/setup_nginx.sh
+++ scripts/setup_nginx.sh
@@ -8,8 +8,9 @@ apt update
 apt install -y nginx 
 
 if 
-systemctl start nginx
 systemctl enable nginx
+systemctl start nginx
+
 
 if systemctl status nginx | grep "active(running)"; then
     echo "Nginx is running."

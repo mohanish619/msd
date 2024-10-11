70e9cff1dbb43fb1f8fabb4f073149d9d6e9bcac
mohanish619 <75253869+mohanish619@users.noreply.github.com>
2024-10-11 20:50:36 +0530

    Update setup_nginx.sh

--- scripts/setup_nginx.sh
+++ scripts/setup_nginx.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-
+# Make the script executable
 chmod +x /home/ubuntu/scripts/setup_nginx.sh
 
-#install nginx
+# Update package lists
 apt update
-apt install -y nginx 
 
-if 
+# Install nginx and check if the installation was successful
+if apt install -y nginx; then
+    echo "Nginx installed successfully."
+else
+    echo "Failed to install Nginx."
+    exit 1
+fi
+
+# Start nginx and enable it to start on boot
 systemctl start nginx
 systemctl enable nginx
 
-if systemctl status nginx | grep "active(running)"; then
+# Check the status of nginx service
+if systemctl status nginx | grep "active (running)"; then
     echo "Nginx is running."
 else
     echo "Failed to start Nginx."
